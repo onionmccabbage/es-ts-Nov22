0fbc6e2d0241e71d70d5535f5957a788ddcfbebe
onionmccabbage <onionmccabbage@live.com>
2022-11-02 14:37:43 +0000

    Add files via upload

---
+++ review1/review1.ts
@@ -0,0 +1,82 @@
+const todos:ToDo[] = [ // or Array<ToDo>
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 7,
+      "title": "illo expedita consequatur quia in",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 8,
+      "title": "quo adipisci enim quam ut ab",
+      "completed": true
+    }]
+
+interface ToDo {
+    userId:number
+    id:number
+    title:string
+    completed:boolean | 'archived'
+}
+
+// consume some ToDo items
+// console.log(typeof (todos)) // object with ONE member called 'todos'
+const job1:ToDo = todos[0] // or we could let Typescript infer the type
+console.log(job1)
+
+// Here is a sort function
+const compareJobByDesc = (a:ToDo, b:ToDo) => { // in descending order
+    if (a.userId < b.userId) {
+        return 1;
+    }
+    if (a.userId > b.userId) {
+        return -1;
+    }
+    return 0;
+}
+
+// here is a filter function
+const filterByUserId = (a:ToDo)=>{
+    if (a.userId === 1) {
+        return a
+    }
+}
+// sort and filter the array
+console.log(todos.sort(compareJobByDesc))
+console.log(todos.filter(filterByUserId))
